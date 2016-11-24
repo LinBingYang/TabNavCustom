3f04936ef911f4884d471cdb1bf55fdda8390b53
Admin <admin@ltekimac-mini.local>
2016-11-24 15:04:51 +0800

    Initial Commit

---
+++ masy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  masy
+//
+//  Created by Admin on 16/11/24.
+//  Copyright © 2016年 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ masy/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  masy
+//
+//  Created by Admin on 16/11/24.
+//  Copyright © 2016年 Admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
